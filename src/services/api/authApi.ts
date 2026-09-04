521c236f4058352b9a38dc14f493e72714b47d64
Fernando Nazário <fernando.nazario.dev@gmail.com>
2026-09-04 16:58:31 -0300

    fix: type erros and returns

---
+++ src/services/api/authApi.ts
@@ -0,0 +1,48 @@
+import { AuthResponse } from "@/src/types/authResponse";
+import { Credentials } from "@/src/types/credentials";
+import axios from "axios";
+import { saveToken, readToken } from "../storage/tokenStorage";
+import { User } from "@/src/types/user";
+import { BASE_URL } from "./baseUrl";
+
+export async function auth(credentials: Credentials) : Promise<AuthResponse> {
+    const END_POINT = "/auth/login";
+
+    try {
+        const response = await axios.post<AuthResponse>(`${BASE_URL}${END_POINT}`, credentials);
+
+        await saveToken(response.data.accessToken);
+
+        return response.data;
+    } catch(error) {
+        if(axios.isAxiosError(error)) {
+            console.error("Request error:", error.response?.status ?? error.message);
+        }
+        throw error;
+    }
+}
+
+export async function validateAccess() {
+    const accessToken = await readToken();
+
+    const END_POINT = "/auth/me"
+
+    if(!accessToken) {
+        throw new Error("Access Token does not exist")
+    }
+
+    try {
+        const response = await axios.get<User>(`${BASE_URL}${END_POINT}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+
+        return response.data;
+    } catch(error) {
+        if(axios.isAxiosError(error)) {
+            console.error("Token is not valid:", error.response?.status ?? error.message);
+        }
+        throw error;
+    }
+}
