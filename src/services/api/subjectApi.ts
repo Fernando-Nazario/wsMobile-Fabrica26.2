16d3263bcdb8d5bd126927d67c4c1fe30ff7326c
Fernando Nazário <fernando.nazario.dev@gmail.com>
2026-09-04 17:05:59 -0300

    feat: created subjectApi

---
+++ src/services/api/subjectApi.ts
@@ -0,0 +1,45 @@
+import { Subject } from "@/src/types/subject";
+import { validateAccess } from "./authApi";
+import { readToken } from "../storage/tokenStorage";
+import axios from "axios";
+import { BASE_URL } from "./baseUrl";
+
+export async function getSubjects() : Promise<Subject[]> {
+    const accessToken = await validateAccess();
+    const END_POINT = "/subjects"
+
+    try {
+        const response = await axios.get<Subject[]>(`${BASE_URL}${END_POINT}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+
+        return response.data;
+    } catch(error) {
+        if(axios.isAxiosError(error)) {
+            console.error("Failed to retrieve subjects:", error.response?.status ?? error.message);
+        }
+        throw error;
+    }
+}
+
+export async function getSingleSubject(id : string) : Promise<Subject> {
+    const accessToken = await validateAccess();
+    const END_POINT = `/subjects/${id}`;
+
+    try {
+        const response = await axios.get<Subject>(`${BASE_URL}${END_POINT}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+
+        return response.data;
+    } catch(error) {
+        if(axios.isAxiosError(error)) {
+            console.error("Failed to retrieve single subject:", error.response?.status ?? error.message);
+        }
+        throw error;
+    }
+}
