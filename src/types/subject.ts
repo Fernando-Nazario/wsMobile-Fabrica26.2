30099754d721bf0026e9a67d8774dbeb0b0247e9
Fernando Nazário <fernando.nazario.dev@gmail.com>
2026-09-05 14:22:39 -0300

    fix: update subject schema

--- src/types/subject.ts
+++ src/types/subject.ts
@@ -1,8 +1,9 @@
 type SubjectStatus = "active" | "pending" | "inactive";
 
 export interface Subject {
-    id: string,
-    name: string,
-    description: string,
-    status: SubjectStatus
+  id:          string;         
+  name:        string;         
+  description: string;         
+  status:      SubjectStatus;
+  coverUrl:    string;         
 }
