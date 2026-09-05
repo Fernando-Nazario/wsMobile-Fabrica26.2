8689b542abe0d79e779f648b8d392c2ce14acb13
Fernando Nazário <fernando.nazario.dev@gmail.com>
2026-09-05 20:50:35 -0300

    feat: enhance RootLayout with modal screen options and back navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,7 @@
-import { Stack } from "expo-router";
+import { palette } from "@/src/constants/palette";
+import { Ionicons } from "@react-native-vector-icons/ionicons";
+import { router, Stack } from "expo-router";
+import { TouchableOpacity } from "react-native";
 import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
@@ -7,7 +10,21 @@ export default function RootLayout() {
             <Stack>
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                 <Stack.Screen name="login" options={{ presentation: "card", title: "Login" }} />
-                <Stack.Screen name="subject/[id]" options={{ presentation: "modal" }} />
+                <Stack.Screen
+                    name="subject/[id]"
+                    options={{
+                        presentation: "modal",
+                        title: "Detalhes",
+                        headerStyle: { backgroundColor: palette.surface },
+                        headerTintColor: palette.text,
+                        headerShadowVisible: false,
+                        headerLeft: () => (
+                            <TouchableOpacity onPress={() => router.back()}>
+                                <Ionicons name="arrow-back" size={24} color={palette.text} />
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
             </Stack>
             <Toast />
         </>
