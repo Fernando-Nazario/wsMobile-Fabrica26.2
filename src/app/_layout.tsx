54a4ee502793992e7b50f5a056474b08745fa749
Fernando Nazário <fernando.nazario.dev@gmail.com>
2026-09-05 15:35:56 -0300

    feat: created root layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import Toast from 'react-native-toast-message';
+
+export default function RootLayout() {
+    return(
+        <>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                <Stack.Screen name="login" options={{presentation: "card"}}/>
+                <Stack.Screen name="subject/[id]" options={{presentation: "modal"}}/>
+            </Stack>
+            <Toast/>
+        </>
+    );
+}
